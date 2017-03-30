e8736039c3bd0017310d2e2c2cb9c6edbcd1ee42
Nicolas Lelievre <lelievnm@mcmaster.ca>
2017-03-30 18:41:29 -0400

    Initial commit

---
+++ ScaryMeter/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ScaryMeter.Startup))]
+namespace ScaryMeter
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
