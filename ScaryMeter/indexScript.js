94e0a09be63d23b22be70fe2d195b2ea82abf424
ChrisGagel <cgagel77@gmail.com>
2019-02-04 18:54:57 +1100

    Created an early slider for user-generated ratings

--- ScaryMeter/indexScript.js
+++ ScaryMeter/indexScript.js
@@ -187,3 +187,21 @@ $(function() {
             }, 1200);
     });
 });
+
+
+
+
+
+
+
+
+// TEST SLIDER FOR RATING MOVIES - https://www.w3schools.com/howto/howto_js_rangeslider.asp
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
